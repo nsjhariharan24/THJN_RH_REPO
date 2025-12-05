a750f3aa2cf1c7bd43e3d35baa5a9bd129d1fa44
nsjhariharan24 <nsjhariharan24@gmail.com>
2025-12-05 12:40:09 +0530

    Initial commit: task3a package without VSCode cache

---
+++ task3a/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'task3a'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hari',
+    maintainer_email='nsjhariharan24@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'task3a_rh_detect=task3a.task3a_rh_detect:main',
+        ],
+    },
+)
